2d3580c73f5025e508c753afa6b44d40d89b07c6
hassan11196 <hassan11196@hotmail.com>
2020-12-10 16:31:33 +0500

    add template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 
 app = Flask(__name__)
@@ -9,6 +9,10 @@ app.config.from_object(Config)
 @app.route('/')
 def hello():
     return "hello world"
+
+@app.route("/template")
+def template_view():
+    return render_template("first.html")
 
 
 if __name__=='__main__':
--- config.py
+++ config.py
@@ -5,3 +5,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = True
     SECRET_KEY = 'SOME_RANDOM_KEY'
+    TEMPLATES_AUTO_RELOAD = True
+    
